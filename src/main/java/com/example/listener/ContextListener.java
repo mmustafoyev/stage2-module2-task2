f6985aadcc11b6bd41c93c3e2bdb1bb32a8253db
mmustafoyev <mmustafoyev123@gmail.com>
2023-08-22 10:02:00 +0500

    commit message

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -3,8 +3,10 @@ package com.example.listener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import java.time.LocalDateTime;
 
+@WebListener
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
