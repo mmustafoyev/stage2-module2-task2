79f8cdb672f2f3410b943eda817a0114de9f6a4a
mmustafoyev <mmustafoyev123@gmail.com>
2023-08-22 11:10:38 +0500

    testGetLoginWithoutAuthUser

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -19,7 +19,9 @@ public class LoginServlet extends HttpServlet{
         HttpSession session = req.getSession(false);
         if(session != null && session.getAttribute("user") != null)
             resp.sendRedirect("login.jsp");
-        else
+        else if (req.getParameter("login").isBlank()) {
+            resp.sendRedirect("login.jsp");
+        } else
             resp.sendRedirect("/user/hello.jsp");
     }
 
